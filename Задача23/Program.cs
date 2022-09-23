01d35ca94ed15ec13e466b2a10cde66b50714199
nikita sidorin <nikittoss12@mail.ru>
2022-09-24 00:13:37 +0700

    задача23

---
+++ Задача23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+GetDashbord(num);
+
+void GetDashbord(int num)
+{
+    int count = 1;
+    while (count <= Math.Abs(num))
+    {
+        Console.WriteLine($"Куб числа {count} ={count * count *count}");
+        count++;
+    }
+}
